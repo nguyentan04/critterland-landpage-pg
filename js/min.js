1a69dbd350bdfeee716718151b9d2a5ef1803032
he <tan@gmail.com>
2022-01-09 23:39:20 +0700

    update

--- js/min.js
+++ js/min.js
@@ -29,6 +29,7 @@ window.onscroll = function() {
     }
 
     setTimeout(function(){ reveal(); }, TIME_OUT);
+    setTimeout(function(){ roadmapEffect(); }, TIME_OUT);
 }
 
 //Main function
@@ -92,6 +93,22 @@ topFunction = function() {
 
 reveal = function() {
     var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+roadmapEffect = function() {
+    var reveals = document.querySelectorAll(".roadmapEffect");
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
